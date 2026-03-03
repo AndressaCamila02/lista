0b911b1cde9b72c351110abf973bb46f7363ff37
AndressaCamila02 <andressa.rosa.pinto@escola.pr.gov.br>
2026-03-03 09:26:07 -0300

    wsdfsdf

--- supabaseClient.js
+++ supabaseClient.js
@@ -4,5 +4,5 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
   'https://izqrqllggskivqxcojfs.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6cXJxbGxnZ3NraXZxeGNvamZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE2MDEsImV4cCI6MjA4NzY4NzYwMX0.X6QJoBIo_coFXxw7C7EyQwXuEzTIPkCwPlefbiWZx_Iz'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6cXJxbGxnZ3NraXZxeGNvamZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMTE2MDEsImV4cCI6MjA4NzY4NzYwMX0.X6QJoBIo_coFXxw7C7EyQwXuEzTIPkCwPlefbiWZx_I'
 )
